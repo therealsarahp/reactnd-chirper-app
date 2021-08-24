5a924009904e9a7e7743319973d4dd3de4bd11cf
Sarah P <poultonse@gmail.com>
2021-08-24 11:17:05 -0500

    loading bar added with react-redux-loading

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import {connect} from "react-redux";
 import {handleInitialData} from "../actions/shared";
 import Dashboard from "./Dashboard";
+import LoadingBar from "react-redux-loading";
 
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <LoadingBar />
         {this.props.loading === true
         ? null
         : <Dashboard />}
