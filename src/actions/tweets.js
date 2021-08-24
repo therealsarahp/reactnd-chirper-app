a580beb9ae3b75357e7da6df88a7b80d86cf64c0
Sarah P <poultonse@gmail.com>
2021-08-24 12:27:01 -0500

    NewTweet component with action/apicall added

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,7 +1,9 @@
-import { saveLikeToggle } from "../utils/api";
+import {saveLikeToggle, saveTweet} from "../utils/api";
+import { showLoading, hideLoading } from "react-redux-loading";
 
 export const RECEIVE_TWEETS = "RECEIVE_TWEETS";
 export const TOGGLE_TWEET = "TOGGLE_TWEET";
+export const ADD_TWEET = "ADD_TWEET";
 
 
 export function receiveTweets(tweets) {
@@ -21,6 +23,13 @@ function toggleTweet({ id, authedUser, hasLiked }){
 
 }
 
+function addTweet(tweet){
+    return{
+        type: ADD_TWEET,
+        tweet,
+    }
+}
+
 export function handleToggleTweet(info){
     return (dispatch) => {
         dispatch(toggleTweet(info))
@@ -31,5 +40,28 @@ export function handleToggleTweet(info){
                 dispatch(toggleTweet(info))
                 alert('There was an error liking the tweet. Try Again')
             })
+    }
+}
+
+export function handleAddTweet(text, replyingTo){
+    return(dispatch, getState)=>{
+        const { authedUser } = getState()
+
+        dispatch(showLoading())
+
+
+        return saveTweet({
+            text,
+            author: authedUser,
+            replyingTo,
+        } )
+            .then((tweet)=> dispatch(addTweet(tweet)))
+            .then(dispatch(hideLoading()))
+            // .catch((e)=>{
+            //     console.warn("Error in handleAddTweet: ", e)
+            //     dispatch(addTweet(tweet))
+            //     alert('There was an error saving the tweet. Try Again')
+            // })
+
     }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import {connect} from "react-redux";
 import {handleInitialData} from "../actions/shared";
 import Dashboard from "./Dashboard";
 import LoadingBar from "react-redux-loading";
+import NewTweet from "./NewTweet";
 
 
 class App extends Component {
@@ -16,10 +17,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <LoadingBar />
-        {this.props.loading === true
-        ? null
-        : <Dashboard />}
+        <NewTweet />
+        {/*<LoadingBar />*/}
+        {/*{this.props.loading === true*/}
+        {/*? null*/}
+        {/*: <Dashboard />}*/}
       </div>
     )
   }
